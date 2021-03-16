6e3bd5be32cf251ea9b47f3df77016d72ae17e6c
Dongjian <dongjian@ysstech.com>
2021-03-16 18:16:53 +0800

    工程资源收集

--- src/test/java/com/dong/PraticeDailyApplicationTests.java
+++ src/test/java/com/dong/PraticeDailyApplicationTests.java
@@ -1,13 +1,30 @@
 package com.dong;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.web.method.HandlerMethod;
+import org.springframework.web.servlet.mvc.method.RequestMappingInfo;
+import org.springframework.web.servlet.mvc.method.RequestMappingInfoHandlerMapping;
+import org.springframework.web.servlet.mvc.method.annotation.RequestMappingHandlerMapping;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 @SpringBootTest
 class PraticeDailyApplicationTests {
 
+	@Autowired
+	List<RequestMappingInfoHandlerMapping> requestMappingInfoHandlerMappings;
 	@Test
 	void contextLoads() {
 	}
 
+	@Test
+	public void testHanlder() {
+		final Map<RequestMappingInfo, HandlerMethod> handlerMethods = new HashMap<>(32);
+		requestMappingInfoHandlerMappings.stream().map(RequestMappingInfoHandlerMapping::getHandlerMethods).forEach(handlerMethods::putAll);
+		System.out.println("工程接口数量：" + handlerMethods.size());
+	}
 }
